20b6797fa96724d51446198b64b1099bb3f4025a
Christoph, René Pardon <pardon@acrontum.de>
2017-11-28 12:37:03 +0100

    formatting; base routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 
+import { RouterModule, Routes } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: '**', redirectTo: 'AppComponent' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // For debugging
+    ),
     BrowserModule,
     FormsModule,
     NgbModule.forRoot()
